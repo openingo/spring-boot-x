35b469ae92c5539416258c262f1b1acda84af05e
Qicz <qiczzhu@gmail.com>
2020-07-15 22:40:48 +0800

    add datasource routing

---
+++ src/main/java/org/openingo/spring/datasource/HikariRoutingDataSource.java
@@ -0,0 +1,58 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 OpeningO Co.,Ltd.
+ *
+ *    https://openingo.org
+ *    contactus(at)openingo.org
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package org.openingo.spring.datasource;
+
+import com.zaxxer.hikari.HikariDataSourceX;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSourceX;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * HikariRoutingDataSource
+ *
+ * @author Qicz
+ */
+public class HikariRoutingDataSource extends AbstractRoutingDataSourceX<HikariDataSourceX> {
+
+    public HikariRoutingDataSource(HikariDataSourceX defaultTargetDataSource,
+                                   ConcurrentHashMap<Object, HikariDataSourceX> targetDataSources) {
+        super(defaultTargetDataSource, targetDataSources);
+    }
+
+    /**
+     * Determine the current lookup key. This will typically be
+     * implemented to check a thread-bound transaction context.
+     * <p>Allows for arbitrary keys. The returned key needs
+     * to match the stored lookup key type, as resolved by the
+     * {@link #resolveSpecifiedLookupKey} method.
+     */
+    @Override
+    protected Object determineCurrentLookupKey() {
+        return null;
+    }
+}
